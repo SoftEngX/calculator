101893217b3addbc10adde4a41013bcf189c9b74
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2025-10-22 23:33:55 +0600

    Add js function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 
+let display = document.querySelector('.display');
+
+let buttons = Array.from(document.querySelectorAll('.button'));
+
+buttons.map((button) => {
+  button.addEventListener('click', (e) => {
+    switch (e.target.innerText) {
+      case 'AC':
+        display.innerText = '0';
+        break;
+      default:
+        if (display.innerText === '0') {
+          display.innerText = e.target.innerText;
+        } else {
+          display.innerText += e.target.innerText;
+        }
+    }
+  });
+});
