13fba91d5ec62b694b921cd090156b0553954bc3
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2025-10-23 23:53:35 +0600

    Add js

--- js/main.js
+++ js/main.js
@@ -9,6 +9,20 @@ buttons.map((button) => {
       case 'AC':
         display.innerText = '0';
         break;
+        case "=":
+         try {
+           display.innerText = eval(display.innerText);
+         } catch (e) {
+          display.innerText = 'Error!';
+         }
+          break;
+          case '+/-':
+            display.innerText = '-';
+            break;
+          case '%':
+            let passedText = display.innerText + '/100';
+            display.innerText = eval(passedText);
+            break;
       default:
         if (display.innerText === '0') {
           display.innerText = e.target.innerText;
